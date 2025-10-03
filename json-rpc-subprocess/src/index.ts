93857009fdd5c8eb06d5961bec78d831cf507090
Andrew Fischer <andy.fischer@gmail.com>
2025-10-02 20:07:56 -0600

    delete: json-rpc-subprocess

--- json-rpc-subprocess/src/index.ts
+++
@@ -1,4 +0,0 @@
-export { ProcessExitWhileWaitingForResponse } from './errors';
-export { JsonRpcSubprocess } from './JsonRpcSubprocess';
-export type { JsonRpcSubprocessOptions } from './JsonRpcSubprocess';
-export type { JsonRpcRequest, JsonRpcResponse } from './JsonRpcTypes';
